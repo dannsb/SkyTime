0fb3201980869d08d1fb34cc12c33541e20605f0
dann <dannforsb@gmail.com>
2023-06-05 12:07:08 +0330

    create skyobjects folder and move some files

---
+++ skyobjects/moon/moon.py
@@ -0,0 +1,12 @@
+import json
+
+# moonPhasesJson = open("./moonphases.json")
+# moonPhases = json.load(moonPhasesJson)
+
+class moon:
+    def moonPhase():
+        pass
+    def moonRise():
+        pass
+    def moonSet():
+        pass
---
+++ skyobjects/sun/sun.py
@@ -0,0 +1,8 @@
+def sunSet():
+    pass
+def sunRise():
+    pass
+def dayLength(dayOfYear):
+    print(dayOfYear)
+
+    pass
