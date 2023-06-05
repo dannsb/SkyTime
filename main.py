488e11c73af8a67157ac1ec9bae1604dc97c4fa5
dann <dannforsb@gmail.com>
2023-06-05 12:07:40 +0330

    add print banner, calculate day of year function

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-import json
+from skyobjects.moon import moon
+from skyobjects.sun import sun
+from datetime import date
+from persiantools.jdatetime import JalaliDate
+from os import system
 
-moonPhasesJson = open("./moonphases.json")
-moonPhases = json.load(moonPhasesJson)
-lunarDay = float(input("Write the lunar day to show the approximate time of moonrise: "))
+def printBanner():
+    system("cls")
+    with open('banner.txt', 'r') as f:
+        for line in f:
+            print(line.rstrip())
+printBanner()
 
-try:
-    time_dict = {0: "06:55", 1: "07:55", 3: "08:30", 4: "09:05", 5: "09:40", 6: "10:30", 7: "11:30", 8: "12:15", 9: "13:15", 10: "14:10", 11: "15:10", 12: "16:05", 13: "17:05", 14: "18:05", 15: "19:05", 16: "19:45", 17: "20:45", 18: "21:46", 19: "22:55", 20: "00:00", 21: "01:05", 22: "02:05", 23: "02:58", 24: "03:43", 25: "04:25", 26: "05:00", 27: "05:31", 28: "06:00", 29: "06:32"}
-    newDict = {}
-    
-    if lunarDay in time_dict:
-        print(time_dict[lunarDay])
-    else:
-        print("Error")
-except ValueError:
-    print("just enter number")
+inputDate = input("\nPlease enter your date (leave it empty for today) : ")
+if not inputDate:
+    inputDate = JalaliDate.today().strftime("%Y/%m/%d")
+
+def calcDayOfYear(date,dateType="persian"):
+    month = int(date.split("/")[1])
+    day = int(date.split("/")[2])
+    dayOfYear = 0
+    counter = 1
+    while counter<month:
+        if(counter <=6):
+            dayOfYear += 31
+        else:
+            dayOfYear += 30
+        counter += 1
+    dayOfYear = dayOfYear + day
+    return dayOfYear
+
+sun.dayLength(calcDayOfYear(inputDate))
