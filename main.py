b440fada61008701cf7d89b75cf7b5c086736778
dann <dannforsb@gmail.com>
2023-06-23 23:54:15 +0330

    add getlunar function , time api

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from skyobjects.moon import moon
 from skyobjects.sun import sun
-from datetime import date
+from datetime import datetime
 from persiantools.jdatetime import JalaliDate
+from progress.spinner import MoonSpinner
 from os import system
+import threading
+from time import sleep
 
 
 def printBanner():
@@ -12,16 +15,7 @@ def printBanner():
             print(line.rstrip())
 
 
-printBanner()
-
-today=JalaliDate.today().strftime("%Y/%m/%d")
-inputDate = input(f"\nPlease enter your date [{today}] : ")
-if not inputDate:
-    inputDate =today
-
-def calcDayOfYear(date, dateType="persian"):
-    month = int(date.split("/")[1])
-    day = int(date.split("/")[2])
+def calcDayOfYear(month, day, dateType="persian"):
     dayOfYear = 0
     counter = 1
     while counter < month:
@@ -34,14 +28,54 @@ def calcDayOfYear(date, dateType="persian"):
     return dayOfYear
 
 
-dayLength = sun.dayLength(calcDayOfYear(inputDate), 30)
-sunRiseTime = sun.sunRise(dayLength)
-sunSetTime = sun.sunSet(dayLength, sunRiseTime)
+def jalilToTimestamp(year, month, day):
+    jalilFormat = JalaliDate(year, month, day)
+    gregorianDate = JalaliDate.to_gregorian(jalilFormat)
+    dateFormat = datetime(gregorianDate.year, gregorianDate.month, gregorianDate.day)
+    timestamp = int(datetime.timestamp(dateFormat))
+    return timestamp
 
-print(
-    f"""
-date : {inputDate}
-sun rise : {sunRiseTime}
-sun set : {sunSetTime}
-"""
-)
+
+def calDatas():
+    global lunarDate, sunRiseTime, sunSetTime
+    year = int(splitedDate[0])
+    month = int(splitedDate[1])
+    day = int(splitedDate[2])
+    timestamp = jalilToTimestamp(year, month, day)
+    lunarDate = moon.getLunarDay(timestamp)
+    dayLength = sun.dayLength(calcDayOfYear(month, day), 30)
+    sunRiseTime = sun.sunRise(dayLength)
+    sunSetTime = sun.sunSet(dayLength, sunRiseTime)
+
+
+try:
+    printBanner()
+    today = JalaliDate.today().strftime("%Y/%m/%d")
+    userDate = input(f"\nPlease enter your date [{today}] : ")
+    if not userDate:
+        userDate = today
+    splitedDate = userDate.split("/")
+
+    spinner = MoonSpinner("Loading ")
+
+    thread = threading.Thread(target=calDatas)
+    thread.start()
+    while thread.is_alive():
+        spinner.next()
+        sleep(0.06)
+    thread.join()
+
+    print(
+        f"""\n
+    date : {userDate}
+    lunar date : {lunarDate}
+    sun rise : {sunRiseTime}
+    sun set : {sunSetTime}
+    """
+    )
+
+except KeyboardInterrupt:
+    exit()
+
+except Exception as err:
+    print(err)
--- skyobjects/moon/moon.py
+++ skyobjects/moon/moon.py
@@ -1,12 +1,25 @@
 import json
+from requests import get
 
-# moonPhasesJson = open("./moonphases.json")
-# moonPhases = json.load(moonPhasesJson)
 
-def moonPhase():
+def getLunarDay(timestamp):
+    try:
+        result = get("https://api.keybit.ir/time/", {"timestamp": timestamp}).json()
+        return result["date"]["other"]["ghamari"]["usual"]["en"]
+
+    except Exception as err:
+        print(err)
+
+
+def moonPhase(lunarDay):
+    moonPhasesJson = open("./moonphases.json")
+    moonPhases = json.load(moonPhasesJson)
     pass
+
+
 def moonRise():
     pass
+
+
 def moonSet():
     pass
-
